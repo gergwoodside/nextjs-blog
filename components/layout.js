740933eec10510f8d5292521c9d40fd7ad7fede7
Greg Woodside <greg.woodside@backstopsolutions.com>
2023-09-08 19:52:27 -0500

    add blog post list

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Greg Woodside';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Greg Woodside | Blog';
 
 export default function Layout({ children, home }) {
   return (
